8de79acd0c3bfbd225ec1f02f694285803c6e2a5
Rafly <rayhansyah817@gmail.com>
2025-01-03 09:18:56 +0700

    video commit

---
+++ src/services/video/repositories/index.js
@@ -0,0 +1,13 @@
+const GetAllVideoRepository = require("./GetAllVideoRepository");
+const GetDetailVideoRepository = require("./GetDetailVideoRepository");
+const EditVideoRepository = require("./EditVideoRepository");
+const CreateVideoRepository = require("./CreateVideoRepository");
+const DeleteVideoRepository = require("./DeleteVideoRepository");
+
+module.exports = {
+  GetAllVideoRepository,
+  GetDetailVideoRepository,
+  EditVideoRepository,
+  CreateVideoRepository,
+  DeleteVideoRepository
+};

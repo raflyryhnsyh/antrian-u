fb8629ce8997e8e94d84f7415a366be9959b3f32
Rafly <rayhansyah817@gmail.com>
2025-01-03 09:20:28 +0700

    pelayanan commit

---
+++ src/services/pelayanan/repositories/index.js
@@ -0,0 +1,12 @@
+const GetAllLoketRepository = require("./GetAllLoketRepository");
+const GetAllPelayananRepository = require("./GetAllPelayananRepository");
+const GetOperatorLoketRepository = require("./GetOperatorLoketRepository");
+const GetPelayananRepository = require("./GetPelayananRepository");
+const GetSubPelayananRepository = require("./GetSubPelayananRepository");
+module.exports = {
+  GetAllLoketRepository,
+  GetAllPelayananRepository,
+  GetOperatorLoketRepository,
+  GetPelayananRepository,
+  GetSubPelayananRepository
+};
